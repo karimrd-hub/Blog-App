6adf5974d4f95987fbba9ed00e6d5a6aa0b7bb07
alijaber2004 <119969405+alijaber2004@users.noreply.github.com>
2025-02-13 20:19:43 +0200

    Update blog.sql

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 17, 2025 at 04:59 PM
+-- Generation Time: Feb 03, 2025 at 09:25 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,53 @@ SET time_zone = "+00:00";
 --
 -- Database: `blog`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `title`, `description`) VALUES
+(3, 'Wild Life', 'Wild life description'),
+(4, 'Travel', 'Travel Description'),
+(5, 'Art', 'Art Description'),
+(6, 'Uncategorized', 'This is the description for the uncategorized category'),
+(8, 'Food', 'Food description');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `thumbnail` varchar(255) NOT NULL,
+  `date_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `category_id` int(11) UNSIGNED DEFAULT NULL,
+  `author_id` int(11) UNSIGNED NOT NULL,
+  `is_featured` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `body`, `thumbnail`, `date_time`, `category_id`, `author_id`, `is_featured`) VALUES
+(2, 'My Title', 'Hello there traveler', '1738571074sky.jpg', '2025-02-02 20:46:02', 4, 7, 1);
 
 -- --------------------------------------------------------
 
@@ -43,11 +90,26 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `email`, `password`, `avatar`, `is_admin`) VALUES
-(1, 'ali', 'jaber', 'alijaber', 'ali@gmail.com', '$2y$10$viFuqlC6bc3bL0y6QWwOWuFC3/W2Fswht60SkWCRq1mSO7LqHQwuS', '17362815221-notebook.png', 0);
+(1, 'ali', 'jaber', 'alijaber', 'ali@gmail.com', '$2y$10$viFuqlC6bc3bL0y6QWwOWuFC3/W2Fswht60SkWCRq1mSO7LqHQwuS', '17362815221-notebook.png', 1),
+(7, 'Mohammad', 'Hussein', 'mhussein', 'mhmd@gmail.com', '$2y$10$VlrJGums0rJkq3.eHfTkkexKu7jbvVAXYD.dqRx1BBkShnlUq8DJm', '1738529096sched.png', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_blog_category` (`category_id`),
+  ADD KEY `FK_blog_author` (`author_id`);
 
 --
 -- Indexes for table `users`
@@ -60,10 +122,33 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `FK_blog_author` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_blog_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
